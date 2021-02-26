58eea71fff74b59b8c610f53f8d93195d0e94537
Alyeev <markmarkul123@gmail.com>
2021-02-26 16:48:26 +0400

    Add Local Carts to Overlay

--- app.js
+++ app.js
@@ -12,6 +12,7 @@ const cartBtn = document.querySelector(".cart-btn"),
 
 // cart
 let cart = [];
+let buttonsDOM = [];
 
 // getting the products
 class Products {
@@ -61,7 +62,8 @@ class UI {
   }
 
   getBagButtons(){
-    const buttons = [...document.querySelectorAll('.bag-btn')]; //format from nodelist to array
+    const buttons = [...document.querySelectorAll('.bag-btn')];
+    buttonsDOM = buttons; //format from nodelist to array
     buttons.forEach(button =>{
        let id = button.dataset.id;
        let inCart = cart.find(item => item.id === id);
@@ -75,16 +77,76 @@ class UI {
           e.target.innerText = "in Cart";
           e.target.disabled = true;
           //! get product from products
+          let cartItem = {...Storage.getProduct(id), amount:1};
           //! add product to the cart
+          cart = [...cart, cartItem];
           //! save cart in local storage
+          Storage.saveCart(cart); 
           //! set cart values
+          this.setCartValues(cart);
           //! display cart item
+          this.addCartItem(cartItem);
           //! show the cart
+          this.showCart();
       });
 
     });
   }
 
+  setCartValues(cart){
+    let tempTotal = 0;
+    let itemsTotal = 0;
+
+    cart.map(item =>{
+       tempTotal += item.price * item.amount;
+       itemsTotal += item.amount;
+    });
+    cartItems.innerText = parseFloat(tempTotal.toFixed(2));
+    cartItems.innerText = itemsTotal;
+  }
+
+  addCartItem(item){
+     const div = document.createElement('div');
+     div.classList.add('cart-item');
+     div.innerHTML = `
+     <img src=${item.image} alt="">
+     <div>
+         <h4>${item.title}</h4>
+         <h5>$${item.price}</h5>
+         <span class="remove-item" data-id=${item.id}>remove</span>
+     </div>
+     <div>
+         <i class="fas fa-chevron-up" data-id=${item.id}></i>
+         <p class="item-amount">${item.amount}</p>
+         <i class="fas fa-chevron-down" data-id=${item.id}></i>
+     </div>
+     `;
+     cartContent.appendChild(div);
+  }
+
+  showCart(){
+    cartOverlay.classList.add('transparentBcg');
+    cartDOM.classList.add('showCart');
+  }
+
+  setupAPP(){
+    cart = Storage.getCart();
+    this.setCartValues(cart);
+    this.populateCart(cart);
+    cartBtn.addEventListener('click', this.showCart);
+    closeCartBtn.addEventListener('click', this.hideCart);
+  }
+
+  populateCart(cart){
+    cart.forEach(item =>{
+      this.addCartItem(item);
+    });
+  }
+
+  hideCart(){
+    cartOverlay.classList.remove('transparentBcg');
+    cartDOM.classList.remove('showCart');
+  }
 }
 
 //local storage
@@ -92,13 +154,27 @@ class Storage {
   static saveProducts(products){
     localStorage.setItem("products", JSON.stringify(products));
   }
+
+  static getProduct(id){
+    let products = JSON.parse(localStorage.getItem('products'));
+    return products.find(product => product.id === id );
+  }
+
+  static saveCart(cart){
+    localStorage.setItem('cart', JSON.stringify(cart));
+  }
+
+  static getCart(){
+    return localStorage.getItem('cart') ? JSON.parse(localStorage.getItem('cart')) : [];
+  }
 }
 
 
 document.addEventListener("DOMContentLoaded", () => {
   const ui = new UI();
   const products = new Products();
-
+  //setup app
+  ui.setupAPP();
   //get all products
   products.getProducts().then(products => {
     ui.displayProducts(products);
@@ -109,8 +185,6 @@ document.addEventListener("DOMContentLoaded", () => {
 
 
 });
-
-
 
 
 
